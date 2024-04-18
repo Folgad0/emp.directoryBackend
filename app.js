0e882eac5253e07501685feef3081facf230f0db
dieegoolimaa <dieegoolimaa@gmail.com>
2024-04-18 14:04:00 +0100

    final details

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 server.use(middlewares);
 server.use(morgan("dev"));
